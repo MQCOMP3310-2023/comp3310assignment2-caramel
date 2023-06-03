280ccc81390fa0ac5eb2dd6e4706ee0b5ad4c207
AidenYoung7701 <aiden.young@students.mq.edu.au>
2023-06-03 16:27:15 +1000

    addition to test file

--- project/authorisation_test.py
+++ project/authorisation_test.py
@@ -1,3 +1,10 @@
 from flask_login import FlaskLoginClient
 
 app.test_client_class = FlaskLoginClient
+
+
+def test_request_with_logged_in_user():
+    user = User.query.get(1)
+    with app.test_client(user=user) as client:
+        # This request has user 1 already logged in!
+        client.get("/")
