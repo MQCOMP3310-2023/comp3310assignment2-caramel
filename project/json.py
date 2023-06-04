e353a6c4ea4d663d79630a35d53dedfb30569986
AidenYoung7701 <aiden.young@students.mq.edu.au>
2023-06-04 14:42:07 +1000

    log messages added as comments

--- project/json.py
+++ project/json.py
@@ -1,3 +1,4 @@
+#import logging
 from flask import Blueprint, jsonify
 from .models import Restaurant, MenuItem
 from sqlalchemy import text
@@ -17,13 +18,20 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
+  #  logging.info( "Selecting resturant:" + restaurant_id )
     items = db.session.execute(text('SELECT * FROM menu_item WHERE restaurant_id = %d', restaurant_id))
 # Adjusted the user input string to query parameter to avoid SQL injection.
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
 
+#It would be a good idea to implement log functions here.
+# this will mean that it is possible to see the restuarnt id after it has been created 
+
+
+
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
+     #  logging.info( "Selecting menu:" + menu_id )
     Menu_Item = db.session.execute(text('SELECT * FROM menu_item WHERE id = %d LIMIT 1', menu_id))
 # Set up a query parameter in code to avoid SQL injection attacks. Used placeholder instead of concatenating
 # the user input variable.
@@ -32,6 +40,7 @@ def menuItemJSON(restaurant_id, menu_id):
 
 @json.route('/restaurant/JSON')
 def restaurantsJSON():
+     #  logging.info( "Selecting all resturants")
     restaurants = db.session.execute(text('select * from restaurant'))
     rest_list = [ r._asdict() for r in restaurants ]
     return pyjs.dumps(rest_list)
